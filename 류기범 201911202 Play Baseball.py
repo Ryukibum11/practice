eff400dfc22feb70fae105f28c4a41086b5fda6a
Ryukibum11 <100761196+Ryukibum11@users.noreply.github.com>
2023-01-03 22:41:29 +0900

    Add files via upload

---
+++ 류기범 201911202 Play Baseball.py
@@ -0,0 +1,36 @@
+#201911202 류기범 PlayBaseball
+
+import random
+
+answer = random.sample(range(0,10),3)
+
+print(answer)
+
+count = 0
+
+while True:
+    strike = 0
+    ball = 0
+    user = input("세자리 숫자를 입력하시오: ")
+    user_list = list(map(int, user))
+    num1 = int(user_list[0])
+    num2 = int(user_list[1])
+    num3 = int(user_list[2])
+    if answer[0] == num1:
+        strike += 1
+    elif answer[0] == num2 or answer[0] == num3:
+        ball += 1
+    if answer[1] == num2:
+        strike += 1
+    elif answer[1] == num1 or answer[1] == num3:
+        ball += 1
+    if answer[2] == num3:
+        strike += 1
+    elif answer[2] == num1 or answer[2] == num2:
+        ball += 1
+    count += 1
+    print("Strike:", strike,"Ball:", ball)
+    if strike == 3:
+        break
+print('You Win!')
+print("You have played",count,"times.")
